1a1a0b34b70e986dd5af56f32820b4da6f91c45b
Isaac <isaac.asensio@gmail.com>
2018-01-17 20:53:46 +0000

    Added memory + refactor
    Modules removed for now.

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,87 +1,44 @@
-pub mod instruction {
 
-    pub struct Instruction {
-        raw: u16
-    }
-
-    impl Instruction {
-        pub fn new(raw: u16) -> Instruction {
-            Instruction {
-                raw: raw
-            }
-        }
-
-        pub fn raw(self) -> u16{
-            self.raw as u16
-        }
-
-        pub fn x(self) -> u8{
-            ((self.raw & 0x0F00) >> 8) as u8
-        }
-
-        pub fn y(self) -> u8{
-            ((self.raw & 0x00F0) >> 4) as u8
-        }
-
-        pub fn nnn(self) -> u16{
-            (self.raw & 0x0FFF) as u16
-        }
-
-        pub fn nn(self) -> u16{
-            (self.raw & 0x00FF) as u16
-        }
-
-        pub fn n(self) -> u16{
-            (self.raw & 0x000F) as u16
-        }
-    }
+pub struct Instruction {
+    raw: u16
 }
 
-
-pub mod executor {
-
-    use super::instruction::Instruction;
-
-    pub struct Executor {
-    }
-
-    impl Executor {
-
-        fn clear_screen(){
-
-        }
-
-        pub fn execute(instruction: Instruction){
-            let val = instruction.raw();
-            match val {
-                0x00E0 => self::clear_screen(),
-                0x00EE => println!("{}", val),
-                _ => println!(""),
-            }
+impl Instruction {
+    pub fn new(raw: u16) -> Instruction {
+        Instruction {
+            raw
         }
     }
-}
 
-#[cfg(test)]
-mod executor_test {
+    pub fn raw(self) -> u16{
+        self.raw as u16
+    }
 
-    use super::executor::Executor;
-    use super::instruction::Instruction;
+    pub fn x(self) -> u8{
+        ((self.raw & 0x0F00) >> 8) as u8
+    }
 
-    #[test]
-    fn op_0x00E0_executes() {
-        for (_, value) in (0xF165..0xF965).enumerate() {
-            let instruction = Instruction::new(value);
-            Executor::execute(instruction);
-        }
+    pub fn y(self) -> u8{
+        ((self.raw & 0x00F0) >> 4) as u8
+    }
+
+    pub fn nnn(self) -> u16{
+        (self.raw & 0x0FFF) as u16
+    }
+
+    pub fn nn(self) -> u16{
+        (self.raw & 0x00FF) as u16
+    }
+
+    pub fn n(self) -> u16{
+        (self.raw & 0x000F) as u16
     }
 }
-
 
 #[cfg(test)]
 mod instruction_test {
 
-    use super::instruction::Instruction;
+    use super::Instruction;
 
     #[test]
     fn x_registry_is_always_the_second_group_of_4_bits() {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-struct Cpu {
-    memory: [u8; 4096],
-
-    vx: [u8; 16], //general purpose registries
-
-    i_reg: u8, // Index registry
-    pc: u16, // Program counter. Currently executing address.
-    sp: u8, // Stack pointer: points to top of the stack
-
-    stack: [u16; 16] // Stores address that interpreter returns when a routine finishes
-    
-}
-
-
 
 fn main() {
     println!("Hello, world!");
